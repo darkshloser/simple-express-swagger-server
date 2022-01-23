c10ae45c7a55d6107cf34a9f8632956622f0c636
darkshloser <dobromir.mail@gmail.com>
2022-01-23 17:53:23 +0200

    Example of swagger documentation of the endpoints to be used latter on

--- index.js
+++ index.js
@@ -16,25 +16,67 @@ const courses = [
 
 const swaggerOptions = {
     swaggerDefinition: {
+        openapi: "3.0.0",
         info: {
             title: "Template swagger documentation",
-            description: "Custom API information",
-            contact: {
-                name: "Name"
-            },
-            server: ["http://localhost:8080"]
-        }
+            version: "0.1.0",
+            description: "Express API documentation",
+        },
+        server: [
+            {
+                url: "http://localhost:8080"
+            }
+        ]  
     },
-    apis: ["index.js"]
+    apis: ["index.js"] 
 }
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+app.use("/api-doc", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
+// Routes
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Book:
+ *       type: object
+ *       required:
+ *         - title
+ *         - author
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the book
+ *         title:
+ *           type: string
+ *           description: the book title
+ *         author:
+ *           type: string
+ *           description: the book title
+ *       example:
+ *         id: jlsad
+ *         title: The new touring omnibus 
+ *         author: Alexander K.
+ */
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Server is working ...');
 })
 
+ /**
+  * @swagger
+  * /api/courses:
+  *   get:
+  *     summery: Return a list with all the courses available
+  *     responses:
+  *       200:
+  *         description: The list of the courses
+  *         content:
+  *           application/json:
+  *             schema:
+  *               type: array
+  */
 app.get('/api/courses', (req, res) => {
     res.send(courses);
 })
